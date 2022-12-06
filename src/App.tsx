10bcba43793427e712aa06d867043a2f5045e113
на работе <andrey.tretiachenko@gmail.com>
2022-12-06 19:01:49 +0300

    add sing in

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="">
-      
+      <a href='https://oauth.vk.com/authorize?client_id=51471477&display=page&redirect_uri=http://localhost:3000&scope=video,groups,offline,stats,stories&response_type=token&v=5.80'>
+        войти ВК
+      </a>
     </div>
   );
 }
