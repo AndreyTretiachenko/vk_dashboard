f5d6ec2f7cdf03bfc03f2a2eb30e2cb82d505242
andreytretiachenko <andrey.tretiachenko@gmail.com>
2022-12-07 23:32:10 +0300

    promise

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { VKget, VKgetStatus } from '../hooks/VKget';
 
 function Groups() {
-  const user = VKget()
   const key = 'vk1.a.35k7vdnZtgzEwhGvjq_Ansf9CZcMGaFemThU7MtcwnENFl5FT_FBG2k57dZpdLpaG-vyukFB7KRS3f88w-KfnDrmbZBBgj7pL392-DOKnFH_4KjHQuIthtqUkDib6lUoH624fOzRDF_i97Cy2No4-v6Gv2q3N_5uxDxyh2la_4p7FGGMqFXh4yPMegpEyJ7SCY5mpDgn61gUyB8IFSL2Mw'
 
   useEffect(() => {
@@ -21,11 +20,13 @@ function Groups() {
     }).catch((err) => {
       console.log(err)
     })
+    VKget().then((res) => {
+      console.log(res)
+    })
   },[])
   
   return (
     <div>
-      <span>{user}</span>
       <span></span>
     </div>
   )
--- src/hooks/VKget.js
+++ src/hooks/VKget.js
@@ -1,9 +1,37 @@
 
 export const VKget = () => {
-    //eslint-disable-next-line no-undef
-    return VK.Api.call('users.get', {user_ids: 478583, v:"5.86"}, function(r) {
-        if(r.response) {
-          console.log(r.response);
-        }
-      });
+    
+     return new Promise((resolve, redject) => {
+      //eslint-disable-next-line no-undef
+      VK.Api.call('groups.getById', {group_id: '173281049', v:"5.86"}, (res) => (
+        resolve(res)
+      ))
+     })
+    
 }
+
+export const VKgetStatus = () => {
+      return new Promise ((resolve, reject) => {
+      //eslint-disable-next-line no-undef
+        VK.Auth.getLoginStatus((res) => {
+          switch (res.status) {
+            case 'connected':
+              resolve(res.session)
+              break
+            case 'not_authorized':
+              reject('error - user dont autorisation')
+              break
+            case 'unknown':
+              resolve(false)
+              break
+            default: return 
+          }
+          
+        }
+      )})
+    }
+        
+      
+      
+
+
