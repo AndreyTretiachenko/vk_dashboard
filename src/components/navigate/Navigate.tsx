4cd10712e0b92f3b0ff6e79bf1a7a632d00c90d9
на работе <andrey.tretiachenko@gmail.com>
2023-01-12 19:09:14 +0300

    5555

--- src/components/navigate/Navigate.tsx
+++ src/components/navigate/Navigate.tsx
@@ -56,7 +56,27 @@ export const Navigate = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              Navbar
+              <div className="">
+                <div>
+                  <img
+                    alt=""
+                    src={data.photo}
+                    style={{border: "1px solid", borderRadius: "100px"}}
+                  />
+                  {data.id ? (
+                    <span className="m-2">
+                      {data.first_name} {data.last_name}
+                    </span>
+                  ) : (
+                    <button
+                      className="btn btn-sm btn-primary m-2"
+                      onClick={handlerOAuthVK}
+                    >
+                      Войти
+                    </button>
+                  )}
+                </div>
+              </div>
             </a>
             <button
               className="navbar-toggler"
@@ -73,188 +93,100 @@ export const Navigate = () => {
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link disabled"
-                    href="#"
-                    tabIndex={-1}
-                    aria-disabled="true"
-                  >
-                    Disabled
-                  </a>
-                </li>
-              </ul>
-              <form className="d-flex">
-                <input
-                  className="form-control mr-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-12 col-md-6 d-inline-flex">
-            <div className="">
               <div>
-                <img
-                  alt=""
-                  src={data.photo}
-                  style={{border: "1px solid", borderRadius: "100px"}}
-                />
-                {data.id ? (
-                  <span className="m-2">
-                    {data.first_name} {data.last_name}
-                  </span>
-                ) : (
-                  <button
-                    className="btn btn-sm btn-primary m-2"
-                    onClick={handlerOAuthVK}
-                  >
-                    Войти
-                  </button>
+                {data.id && (
+                  <div className="col-12 col-sm-12 col-md-12 col-lg-6 d-flex ">
+                    <div className="input-group input-group-sm m-2 flex-fill">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon3">
+                          Список групп
+                        </span>
+                      </div>
+                      <select
+                        onChange={e => {
+                          setSelectInputGroup({
+                            id: Number(e.target.value),
+                            name: e.target.options[
+                              e.target.selectedIndex
+                            ].getAttribute("data-name"),
+                            dateEnd: "",
+                            dateStart: "",
+                            photo_100: "",
+                          });
+                        }}
+                        className="form-control"
+                        aria-describedby="basic-addon3"
+                      >
+                        {inputGroup.map(item => (
+                          <option
+                            key={item.id}
+                            value={item.id}
+                            data-name={item.name.toString()}
+                          >
+                            {item.name.trim()}
+                          </option>
+                        ))}
+                      </select>
+                      <FindGroupsByID
+                        pressFind={handlePressFind}
+                        listFind={findListGroup.search}
+                        addGroup={handlerAddGroup}
+                      />
+                    </div>
+
+                    <button
+                      className="d-inline btn btn-sm btn-outline-primary m-2"
+                      onClick={handlerGetStatGroup}
+                    >
+                      Загрузить аналитику
+                    </button>
+
+                    <input
+                      type="checkbox"
+                      id="exampleCheck1"
+                      checked={favourite}
+                      onChange={e => setFavourite(e.target.checked)}
+                    />
+                    <label
+                      className="form-check-label  ml-2"
+                      htmlFor="exampleCheck1"
+                    >
+                      избранное
+                    </label>
+                    <div>
+                      <label className="d-inline pl-4 mx-1">период:</label>
+                      <input
+                        className="form-control d-inline"
+                        style={{width: "20%"}}
+                        type="date"
+                        value={selectInputGroup.dateStart}
+                        onChange={e =>
+                          setSelectInputGroup({
+                            ...selectInputGroup,
+                            dateStart: e.target.value,
+                          })
+                        }
+                      />
+                      <span className="mx-3">-</span>
+                      <input
+                        className="form-control d-inline"
+                        style={{width: "20%"}}
+                        type="date"
+                        value={selectInputGroup.dateEnd}
+                        onChange={e =>
+                          setSelectInputGroup({
+                            ...selectInputGroup,
+                            dateEnd: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                  </div>
                 )}
               </div>
             </div>
           </div>
-          {data.id && (
-            <div className="col-12 col-sm-12 col-md-12 col-lg-6 d-flex ">
-              <div className="input-group input-group-sm m-2 flex-fill">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon3">
-                    Список групп
-                  </span>
-                </div>
-                <select
-                  onChange={e => {
-                    setSelectInputGroup({
-                      id: Number(e.target.value),
-                      name: e.target.options[
-                        e.target.selectedIndex
-                      ].getAttribute("data-name"),
-                      dateEnd: "",
-                      dateStart: "",
-                      photo_100: "",
-                    });
-                  }}
-                  className="form-control"
-                  aria-describedby="basic-addon3"
-                >
-                  {inputGroup.map(item => (
-                    <option
-                      key={item.id}
-                      value={item.id}
-                      data-name={item.name.toString()}
-                    >
-                      {item.name.trim()}
-                    </option>
-                  ))}
-                </select>
-                <FindGroupsByID
-                  pressFind={handlePressFind}
-                  listFind={findListGroup.search}
-                  addGroup={handlerAddGroup}
-                />
-              </div>
-
-              <button
-                className="d-inline btn btn-sm btn-outline-primary m-2"
-                onClick={handlerGetStatGroup}
-              >
-                Загрузить аналитику
-              </button>
-
-              <input
-                type="checkbox"
-                id="exampleCheck1"
-                checked={favourite}
-                onChange={e => setFavourite(e.target.checked)}
-              />
-              <label className="form-check-label  ml-2" htmlFor="exampleCheck1">
-                избранное
-              </label>
-              <div>
-                <label className="d-inline pl-4 mx-1">период:</label>
-                <input
-                  className="form-control d-inline"
-                  style={{width: "20%"}}
-                  type="date"
-                  value={selectInputGroup.dateStart}
-                  onChange={e =>
-                    setSelectInputGroup({
-                      ...selectInputGroup,
-                      dateStart: e.target.value,
-                    })
-                  }
-                />
-                <span className="mx-3">-</span>
-                <input
-                  className="form-control d-inline"
-                  style={{width: "20%"}}
-                  type="date"
-                  value={selectInputGroup.dateEnd}
-                  onChange={e =>
-                    setSelectInputGroup({
-                      ...selectInputGroup,
-                      dateEnd: e.target.value,
-                    })
-                  }
-                />
-              </div>
-            </div>
-          )}
-        </div>
+        </nav>
       </div>
     </>
   );
