e53f1d5bcc2e32720c5c69a8b47d127807879fa8
на работе <andrey.tretiachenko@gmail.com>
2023-01-18 14:38:27 +0300

    123

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
   Area,
   AreaChart,
@@ -13,51 +13,72 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import { useAppSelector } from "../../hooks/hookStore";
+import {useAppSelector} from "../../hooks/hookStore";
+import moment from "moment";
 
 export const Charts = () => {
-  const response = useAppSelector((state) => state.stats);
+  const response = useAppSelector(state => state.stats);
+  let arrDate = new Array(
+    Math.abs(
+      moment.unix(response.end).diff(moment.unix(response.start), "days")
+    )
+  );
+
+  arrDate = [...arrDate].map((item, index) => {
+    return {
+      date: moment.unix(response.start).add({day: index}).format("DD-MM-YYYY"),
+    };
+  });
+
+  const resDate = [...arrDate].map(item => {
+    return {
+      ...item,
+
+      posts: response.items.filter(
+        (post: any) => moment.unix(post.date).format("DD-MM-YYYY") === item.date
+      ).length,
+    };
+  });
+
   return (
     <>
       <div className="card text-center mt-2">
-        <div className="card-header">Количество постов по дням</div>
+        <div className="card-header">Динамика размещения постов за период</div>
         <div className="card-body p-0 mr-3 mt-3 pt-1">
           <ResponsiveContainer width="100%" height={250}>
             <BarChart
               width={730}
               height={250}
-              data={response.items
-                .map((item) => {
-                  return {
-                    дата: new Intl.DateTimeFormat("ru", {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "2-digit",
-                    }).format(item.date * 1000),
-                    посты: item.views ? item.views.count : 0,
-                  };
-                })
-                .reverse()}
+              data={resDate}
               margin={{
                 top: 5,
                 right: 0,
-                left: 0,
+                left: -15,
                 bottom: 5,
               }}
             >
-              <CartesianGrid strokeDasharray="3 3" />
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
               <XAxis
-                dataKey="дата"
-                style={{ fontSize: "0.85rem" }}
-                padding={{ right: 10, left: 0 }}
+                dataKey="date"
+                style={{fontSize: "0.75rem"}}
+                padding={{right: 10, left: 15}}
               />
-              <YAxis />
+              <YAxis
+                interval={1}
+                dataKey="posts"
+                style={{fontSize: "0.85rem"}}
+                padding={{bottom: 10, top: 30}}
+              />
               <Tooltip />
-              <Bar dataKey="посты" fill="#82ca9d" />
+              <Bar
+                dataKey="posts"
+                fill="#017BFE"
+                label={{position: "top", fontSize: "0.75rem"}}
+              />
             </BarChart>
           </ResponsiveContainer>
         </div>
-        <div className="card-footer text-muted"></div>
+        {/* <div className="card-footer text-muted"></div> */}
       </div>
       <div className="card text-center mt-2">
         <div className="card-header">Просмотры постов по дням</div>
@@ -67,7 +88,7 @@ export const Charts = () => {
               width={800}
               height={500}
               data={response.items
-                .map((item) => {
+                .map(item => {
                   return {
                     дата: new Intl.DateTimeFormat("ru", {
                       day: "2-digit",
@@ -81,7 +102,7 @@ export const Charts = () => {
               margin={{
                 top: 5,
                 right: 0,
-                left: 0,
+                left: -15,
                 bottom: 5,
               }}
             >
@@ -94,12 +115,12 @@ export const Charts = () => {
               </defs>
               <XAxis
                 dataKey="дата"
-                style={{ fontSize: "0.85rem" }}
-                padding={{ right: 10, left: 0 }}
+                style={{fontSize: "0.85rem"}}
+                padding={{right: 10, left: 15}}
               />
               <YAxis
-                style={{ fontSize: "0.85rem" }}
-                padding={{ top: 10, bottom: 20 }}
+                style={{fontSize: "0.85rem"}}
+                padding={{top: 10, bottom: 0}}
               />
               <Tooltip />
 
@@ -110,11 +131,12 @@ export const Charts = () => {
                 stroke="#82ca9d"
                 fillOpacity={1}
                 fill="url(#colorUv)"
+                label={{position: "top", fontSize: "0.75rem"}}
               />
             </AreaChart>
           </ResponsiveContainer>
         </div>
-        <div className="card-footer text-muted"></div>
+        {/* <div className="card-footer text-muted"></div> */}
       </div>
       <div className="card text-center mt-2">
         <div className="card-header">
@@ -126,7 +148,7 @@ export const Charts = () => {
               width={800}
               height={500}
               data={response.items
-                .map((item) => {
+                .map(item => {
                   return {
                     дата: new Intl.DateTimeFormat("ru", {
                       day: "2-digit",
@@ -143,7 +165,7 @@ export const Charts = () => {
               margin={{
                 top: 5,
                 right: 0,
-                left: 0,
+                left: -15,
                 bottom: 5,
               }}
             >
@@ -156,14 +178,14 @@ export const Charts = () => {
               </defs>
               <XAxis
                 dataKey="дата"
-                style={{ fontSize: "0.85rem" }}
-                padding={{ right: 10, left: 0 }}
+                style={{fontSize: "0.85rem"}}
+                padding={{right: 10, left: 15}}
               />
               <YAxis
-                style={{ fontSize: "0.85rem" }}
-                padding={{ top: 10, bottom: 20 }}
+                style={{fontSize: "0.85rem"}}
+                padding={{top: 10, bottom: 10}}
               />
-              <Tooltip contentStyle={{ fontSize: "0.85rem" }} />
+              <Tooltip contentStyle={{fontSize: "0.85rem"}} />
 
               {/* <Legend /> */}
               <Line
@@ -171,18 +193,25 @@ export const Charts = () => {
                 dataKey="комментарии"
                 stroke="blue"
                 dot={false}
+                label={{position: "top", fontSize: "0.65rem"}}
               />
-              <Line type="monotone" dataKey="лайки" stroke="red" dot={false} />
+              <Line
+                type="monotone"
+                dataKey="лайки"
+                stroke="red"
+                dot={false}
+                label={{position: "top", fontSize: "0.65rem"}}
+              />
               <Line
                 type="monotone"
                 dataKey="репосты"
                 stroke="#82ca9d"
                 dot={false}
+                label={{position: "top", fontSize: "0.65rem"}}
               />
             </LineChart>
           </ResponsiveContainer>
         </div>
-        <div className="card-footer text-muted"></div>
       </div>
     </>
   );
