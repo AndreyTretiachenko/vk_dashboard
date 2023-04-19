1a27eebf035e907aa47ad3eaec68203b963aa027
Директор IT <meb290@mail.ru>
2023-04-19 12:23:26 +0300

    123

--- src/components/navigate/Navigate.tsx
+++ src/components/navigate/Navigate.tsx
@@ -46,32 +46,24 @@ export const Navigate = () => {
 
   useEffect(() => {
     if (favourite) setinputGroup(favouriteList);
-    else
-      setinputGroup(
-        groupIDs.sort(({ name: a }, { name: b }) => a.localeCompare(b))
-      );
+    else setinputGroup(groupIDs.sort(({ name: a }, { name: b }) => a.localeCompare(b)));
   }, [favourite, favouriteList]);
 
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex align-items-sm-center">
         <div className="mx-1 my-1 d-flex w-20">
-          <img
-            alt="0_0"
-            src={data?.photo}
-            style={{ border: "1px solid", borderRadius: "100px" }}
-          />
+          <img alt="0_0" src={data?.photo} style={{ border: "1px solid", borderRadius: "100px" }} />
           {data.id ? (
             <>
-              <span className="m-2 h6">
-                {data?.first_name} {data?.last_name}
-              </span>
+              <a href="/callback-vk" className="btn bnt-text">
+                <span className="m-2 h6">
+                  {data?.first_name} {data?.last_name}
+                </span>
+              </a>
             </>
           ) : (
-            <button
-              className="btn btn-sm btn-primary m-2"
-              onClick={handlerOAuthVK}
-            >
+            <button className="btn btn-sm btn-primary m-2" onClick={handlerOAuthVK}>
               Войти
             </button>
           )}
@@ -84,8 +76,7 @@ export const Navigate = () => {
           data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -108,27 +99,16 @@ export const Navigate = () => {
                                   onChange={(e) => {
                                     setSelectInputGroup({
                                       id: Number(e.target.value),
-                                      name: e.target.options[
-                                        e.target.selectedIndex
-                                      ].getAttribute("data-name"),
-                                      dateEnd: new Date(
-                                        Date.now()
-                                      ).toLocaleDateString("en-CA"),
-                                      dateStart: new Date(
-                                        Date.now()
-                                      ).toLocaleDateString("en-CA"),
+                                      name: e.target.options[e.target.selectedIndex].getAttribute("data-name"),
+                                      dateEnd: new Date(Date.now()).toLocaleDateString("en-CA"),
+                                      dateStart: new Date(Date.now()).toLocaleDateString("en-CA"),
                                       photo_100: "",
                                     });
                                   }}
                                   className="form-control form-control-sm"
-                                  aria-describedby="basic-addon3"
-                                >
+                                  aria-describedby="basic-addon3">
                                   {inputGroup.map((item) => (
-                                    <option
-                                      key={item.id}
-                                      value={item.id}
-                                      data-name={item.name.toString()}
-                                    >
+                                    <option key={item.id} value={item.id} data-name={item.name.toString()}>
                                       {item.name.trim()}
                                     </option>
                                   ))}
@@ -145,14 +125,9 @@ export const Navigate = () => {
                                     type="checkbox"
                                     id="exampleCheck1"
                                     checked={favourite}
-                                    onChange={(e) =>
-                                      setFavourite(e.target.checked)
-                                    }
+                                    onChange={(e) => setFavourite(e.target.checked)}
                                   />
-                                  <label
-                                    className="form-check-label"
-                                    htmlFor="exampleCheck1"
-                                  >
+                                  <label className="form-check-label" htmlFor="exampleCheck1">
                                     избранное
                                   </label>
                                 </div>
@@ -178,9 +153,7 @@ export const Navigate = () => {
                                 style={{ width: "auto" }}
                                 type="date"
                                 value={selectInputGroup.dateStart}
-                                defaultValue={new Date(
-                                  Date.now()
-                                ).toLocaleDateString("en-CA")}
+                                defaultValue={new Date(Date.now()).toLocaleDateString("en-CA")}
                                 onChange={(e) =>
                                   setSelectInputGroup({
                                     ...selectInputGroup,
@@ -188,17 +161,13 @@ export const Navigate = () => {
                                   })
                                 }
                               />
-                              <span className="mx-1 d-flex align-items-center">
-                                по:
-                              </span>
+                              <span className="mx-1 d-flex align-items-center">по:</span>
                               <input
                                 className="form-control form-control-sm d-inline"
                                 style={{ width: "auto" }}
                                 type="date"
                                 value={selectInputGroup.dateEnd}
-                                defaultValue={new Date(
-                                  Date.now()
-                                ).toLocaleDateString("en-CA")}
+                                defaultValue={new Date(Date.now()).toLocaleDateString("en-CA")}
                                 onChange={(e) =>
                                   setSelectInputGroup({
                                     ...selectInputGroup,
@@ -209,10 +178,7 @@ export const Navigate = () => {
                             </div>
                             <div className="col-12 p-0">
                               <div className="">
-                                <button
-                                  className="btn btn-primary btn-sm my-2"
-                                  onClick={handlerGetStatGroup}
-                                >
+                                <button className="btn btn-primary btn-sm my-2" onClick={handlerGetStatGroup}>
                                   Загрузить аналитику
                                 </button>
                               </div>
